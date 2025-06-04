609d194bd1db2aedb0def66a2acd6319d2a43bb4
Your Name <your-email@example.com>
2025-06-04 22:40:08 +0000

    Add React app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Salchert Answers</h1>
+        <p>This is my new website deployed on AWS Amplify.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
